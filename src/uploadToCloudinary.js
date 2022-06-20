0f86201cd9055b990f6ade6d89b25ae620dcf3d0
nitasn <46115487+nitasn@users.noreply.github.com>
2022-06-20 15:01:08 +0300

    readablity

--- src/uploadToCloudinary.js
+++ src/uploadToCloudinary.js
@@ -1,17 +1,7 @@
-/**
- * @param {string} url
- */
-function removeTrailingSlash(url) {
-  if (url.endsWith('/')) {
-    return url.substring(0, url.length - 1);
-  }
-  return url;
-}
-
 const cloudName = 'lost-and-found-startup';
-const uploadUrl = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
-
 const unsignedPreset = 'mobile-uploads';
+
+const uploadUrl = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
 
 /**
  * @returns {Promise<string?>} url of the hosted image, or null if error
@@ -21,17 +11,17 @@ export default async function uploadToCloudinary(filedata) {
   formData.append('file', filedata);
   formData.append('upload_preset', unsignedPreset);
 
-  // return 'todo-delete-me-debug-url'
-
   try {
     const res = await fetch(uploadUrl, {
       method: 'POST',
       body: formData,
     });
     const json = await res.json();
-    const url = json.url;
-    return removeTrailingSlash(url);
-  } catch (err) {
-    return (console.error(err), null);
+    return json.secure_url;
+    // see response structure at
+    // https://cloudinary.com/documentation/image_upload_api_reference#upload_response
+  } 
+  catch (err) {
+    return console.error('uploadToCloudinary error:', err.message), null;
   }
 }
